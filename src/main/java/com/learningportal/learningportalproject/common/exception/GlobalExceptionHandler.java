75335a187318f897e571ba85b749d92382c405d6
aditiedculcate <106309632+the-aditikumari841@users.noreply.github.com>
2026-03-31 23:48:59 +0530

    Implemented GlobalExceptionHandler for centralized error handling

---
+++ src/main/java/com/learningportal/learningportalproject/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.learningportal.learningportalproject.common.exception;
+
+import com.learningportal.learningportalproject.common.response.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse> handleValidationException(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        ApiResponse response = new ApiResponse(
+                "Validation Failed",
+                "ERROR",
+                LocalDateTime.now(),
+                errors
+        );
+
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ApiResponse> handleRuntimeException(
+            RuntimeException ex) {
+
+        ApiResponse response = new ApiResponse(
+                ex.getMessage(),
+                "ERROR",
+                LocalDateTime.now(),
+                null
+        );
+
+        return ResponseEntity.badRequest().body(response);
+    }
+}
